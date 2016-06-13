acb8fac3916c29974c9654bcd6f14e7f8e3072ee
Tomasz Jaworski <wiedzminek112@gmail.com>
2016-06-13 18:12:02 +0200

    changed delegate used by Dispatcher to System.Action

--- src/Assets/Plugins/PatchKit/Scripts/Common/Dispatcher.cs
+++ src/Assets/Plugins/PatchKit/Scripts/Common/Dispatcher.cs
@@ -1,8 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
-using Action = Newtonsoft.Json.Serialization.Action;
 
 namespace PatchKit.Unity.Common
 {
